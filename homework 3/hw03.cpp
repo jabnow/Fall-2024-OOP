e9916f63c4f8bbc595008371d55091fcff66cccc
Joy <97805745+jabnow@users.noreply.github.com>
2024-09-26 21:34:32 -0400

    Add files via upload
    80 characters
    added better comments and error checking

--- homework 3/hw03.cpp
+++ homework 3/hw03.cpp
@@ -1,5 +1,5 @@
 //
-// Created by jwang on 9/23/2024.
+// Created by Joy Wang on 9/23/2024.
 // Expanded Warrior class with weapons
 //
 
@@ -10,11 +10,23 @@
 using namespace std;
 
 class Warrior{
-    class Weapon;
+    class Weapon{
+        friend ostream& operator<<(ostream& os, const Weapon& sword);
+    public:
+        // actually holds strength, can only be accessed from warrior
+        Weapon(const string& weapon_name,int points) : sword_name(weapon_name),
+        strength(points) {}
+        // setters and getters
+        void setStrength(int points) {strength = points;}
+        int getStrength() const {return strength;}
+        const string& getName() const {return sword_name;}
+    private:
+        string sword_name;
+        int strength;
+    };
     friend ostream& operator<<(ostream& os, const Warrior::Weapon& sword);
     friend ostream& operator<<(ostream& os, const Warrior& some_warrior);
 public:
-    // make warrior, find warrior may become methods?
     Warrior(const string& name, const string& weapon, int strength) :
     warrior_name(name), weapon_name(weapon, strength) {}
     // setters and getters
@@ -22,6 +34,8 @@ public:
     int getStrength() const {return weapon_name.getStrength();}
     const string& getName() const {return warrior_name;}
     // battle is now a method
+    // parameter: passes in another warrior, updates the change in strength
+    // prints out the result of the battle based on who is alive or not
     void battle(Warrior& enemy){
         cout << warrior_name << " battles " << enemy.warrior_name << endl;
         // both are dead
@@ -40,54 +54,44 @@ public:
                 // this one is stronger
                 setStrength(getStrength() - enemy.getStrength());
                 enemy.setStrength(0);
-                cout << warrior_name << " defeats " << enemy.warrior_name << endl;
+                cout << warrior_name << " defeats " << enemy.warrior_name
+                << endl;
             } else if (getStrength() < enemy.getStrength()){
                 // the other is stronger
                 enemy.setStrength(enemy.getStrength() - getStrength());
                 setStrength(0);
-                cout << enemy.warrior_name << " defeats " << warrior_name << endl;
+                cout << enemy.warrior_name << " defeats " << warrior_name <<
+                endl;
             } else {
                 // both are alive and equal
                 enemy.setStrength(0);
                 setStrength(0);
                 cout << "Mutual Annihilation: " << warrior_name << " and " <<
-                     enemy.warrior_name << " die at each other's hands" << endl;
+                     enemy.warrior_name << " die at each other's hands" <<
+                     endl;
             }
         }
     }
 private:
-    class Weapon{
-        friend ostream& operator<<(ostream& os, const Weapon& sword);
-    public:
-        // actually holds strength, can only be accessed from warrior
-        Weapon(const string& weapon_name, int points) : sword_name(weapon_name), strength(points) {}
-        // setters and getters
-        void setStrength(int points) {strength = points;}
-        int getStrength() const {return strength;}
-        const string& getName() const {return sword_name;}
-    private:
-        string sword_name;
-        int strength;
-    };
     string warrior_name;
     Weapon weapon_name;
 
 };
 // overloading nested Weapon operator
 ostream& operator<<(ostream& os, const Warrior::Weapon& sword){
-    os << "weapon: " << sword.sword_name << ", " << sword.strength; // only one operator?
+    os << "weapon: " << sword.sword_name << ", " << sword.strength;
     return os;
 }
 // overloading Warrior operator
 ostream& operator<<(ostream& os, const Warrior& some_warrior){
-    os << "Warrior: " << some_warrior.warrior_name << ", " << some_warrior.weapon_name;
-    // but I friended it :(
+    os << "Warrior: " << some_warrior.warrior_name << ", " <<
+    some_warrior.weapon_name;
     return os;
 }
 
 // function prototypes
 size_t find_warrior(const string& name, const vector<Warrior>& population);
-void status(vector<Warrior>& population);
+void status(const vector<Warrior>& population);
 void process_keyword(ifstream& ifs, vector<Warrior>& population);
 
 int main(){
@@ -103,9 +107,10 @@ int main(){
     return 0;
 }
 
+// searches for the warrior and returns its location in the population vector
 size_t find_warrior(const string& name, const vector<Warrior>& population){
     for (size_t i = 0; i < population.size(); i++) {
-        // check if warrior name is unique, weapons don't matter
+        // check that warrior exists
         if (population[i].getName() == name) {
             return i;
         }
@@ -113,29 +118,41 @@ size_t find_warrior(const string& name, const vector<Warrior>& population){
     return population.size();
 }
 
-void status(vector<Warrior>& population){
+// prints out the total number of warriors, alive and dead
+// prints out their name, weapon, and remaining strength
+void status(const vector<Warrior>& population){
     cout << "There are: " << population.size() << " warriors\n";
     for (const Warrior& some_warrior : population){
         cout << some_warrior << endl;
     }
 }
 
+// takes in the file and Warrior vector and identifies the keywords
+// makes a call depending on the command: Warrior, Battle, Status
 void process_keyword(ifstream& ifs, vector<Warrior>& population){
-    // whether the command is Warrior, Battle, Status
     string keyword, name1, name2, weapon;
     int strength;
     while (ifs >> keyword){
         if (keyword == "Warrior"){
             // construct a Warrior and their Weapon
             ifs >> name1 >> weapon >> strength;
-            population.emplace_back(name1, weapon, strength); // why not create temporary object?
+            // check if warrior name is unique, weapons don't matter
+            for (size_t i=0; i<population.size(); i++){
+                if (population[i].getName() != name1){
+                    population.emplace_back(name1, weapon, strength);
+                } else {
+                    cerr << "warrior name is not unique" << endl;
+                    exit(1);
+                }
+            }
+
         } else if (keyword == "Battle"){
             // call find_and_battle(...)
             ifs >> name1 >> name2;
             size_t w1_place = find_warrior(name1, population);
             size_t w2_place = find_warrior(name2, population);
             // if both exist, then battle and update strengths at location
-            if (w1_place != population.size() && w2_place != population.size()){
+            if (w1_place!=population.size() && w2_place!=population.size()){
                 population[w1_place].battle(population[w2_place]);
             } else {
                 cout << "warriors not found; there was no battle" << endl;
